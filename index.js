2c11e0f6c02767ab3669cd334443982ebc8ad363
hammad rafique <hammadrafique397@gmail.com>
2023-03-13 23:54:14 +0500

    decorator function

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-'use strict';
+const decorator = (func) => {
+  return (...args) => {
+    console.log(`Function name is ${func.name}`);
+    func.call(this, ...args);
+  };
+};
 
-
-
-const decorator = func => {
-    return (...args) => {
-        console.log(`Function name is ${func.name}`);
-        func.call(this, ...args);
-    }
-}
-
-const hello = about => console.log(`This is a ${about}`);
-
+const hello = (about) => console.log(`This is a ${about}`);
 
 const decoratedFunc = decorator(hello);
-decoratedFunc('higher order decorator function');
+decoratedFunc("higher order decorator function");
